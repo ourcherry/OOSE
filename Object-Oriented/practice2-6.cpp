9df4a7bbe4fcaef0f421b65fc041b48c135811ab
Heera <heerajeong92@gmail.com>
2025-04-04 23:14:32 +0900

    practice 2

---
+++ Object-Oriented/practice2-6.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int* GetArray(int num)
+{
+	if (num <= 0)
+	{
+		// null pointer
+		return nullptr;
+	}
+
+	// 동적 메모리 할당, (nothrow) 예외 처리 옵션 -> throw error 대신 nullptr 리턴
+	int* p = new(nothrow) int[num]; 
+
+	if (p == nullptr)
+	{
+		cerr << "메모리 할당 실패" << endl;
+		return nullptr;
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		p[i] = i;
+	}
+	return p;
+}
+
+int main_2_6(void)
+{
+	int i;
+	int* pAry;
+	pAry = GetArray(5);
+
+	for (i = 0; i < 5; i++)
+	{
+		cout << pAry[i] << " ";
+	}
+	cout << endl;
+	delete[] pAry;
+
+	return 0;
+}
---
+++ Object-Oriented/practice2-9.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main_2_9(void)
+{
+	int sum = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		sum += (i + 1);
+	}
+
+	cout << "SUM 1~100 : " << sum << endl;
+
+	return 0;
+}
