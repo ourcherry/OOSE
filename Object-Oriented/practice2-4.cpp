5c5303b98d16621df6cb707a1302984587a82cef
Heera <heerajeong92@gmail.com>
2025-04-04 22:55:51 +0900

    practice 2

---
+++ Object-Oriented/practice2-4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int GetArea(int col, int row)
+{
+	return col * row;
+}
+
+double GetArea(int radius)
+{
+	return radius * radius * 3.14;
+}
+
+int main_2_4(void)
+{
+	int sel;
+	cout << "구하고자 하는 넓이를 선택해주세요. 1) 직사각형 2) 원\n";
+	cin >> sel;
+
+	switch (sel)
+	{
+	case 1:
+		int col, row;
+		cout << "가로, 세로를 순서대로 입력해주세요.\n";
+		cin >> row >> col;
+		cout << "---------직사각형 넓이 : " << GetArea(col, row) << endl;
+		break;
+
+	case 2:
+		int radius;
+		cout << "반지름을 입력해주세요.\n";
+		cin >> radius;
+		cout << "--------원의 넓이 : " << GetArea(radius) << endl;
+		break;
+	}
+
+	return 0;
+}
