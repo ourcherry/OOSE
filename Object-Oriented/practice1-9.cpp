e49fc9dc98c92d32349bfb0b832594e51ae53c14
heera <heera@DESKTOP-7RMAISM>
2025-04-04 20:39:21 +0900

    초기 커밋.

---
+++ Object-Oriented/practice1-9.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+void Print(char *str, int count)
+{
+	if (count <= 0) return;
+
+	printf("%s\n", str);
+	Print(str, count - 1);
+}
+
+int main(void) 
+{
+	Print("recursive function", 5);
+
+	return 0;
+}
